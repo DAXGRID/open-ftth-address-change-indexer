1293239ea03ceee5beac32c4902e58e736055a3c
runeanielsen <runenielsen@runbox.com>
2023-04-04 11:20:44 +0200

    setup project

---
+++ src/OpenFTTH.AddressChangeIndexer/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenFTTH.AddressChangeIndexer;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello!");
+    }
+
+    public static int Setup()
+    {
+        return 1;
+    }
+}
---
+++ test/OpenFTTH.AddressChangeIndexer.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace OpenFTTH.AddressChangeIndexer.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(1, Program.Setup());
+    }
+}
