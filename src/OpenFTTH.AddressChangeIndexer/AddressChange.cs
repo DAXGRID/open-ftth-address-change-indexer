25fd16502f42995ada728ab379044075807077c6
Rune Nielsen <runenielsen@runbox.com>
2023-04-28 21:56:28 +0200

    can now convert unit address changes to address changes

--- src/OpenFTTH.AddressChangeIndexer/AddressChange.cs
+++ src/OpenFTTH.AddressChangeIndexer/AddressChange.cs
@@ -1,31 +1,134 @@
+using OpenFTTH.Core.Address;
+
 namespace OpenFTTH.AddressChangeIndexer;
 
 internal enum AddressChangeType
 {
-    New,
-    CoordinateChanged,
-    Deleted
+    UnitAddressCreated,
+    UnitAddressAccessAddressIdChanged,
+    UnitAddressStatusChanged,
+    UnitAddressFloorNameChanged,
+    UnitAddressSuiteNameChanged,
+    UnitAddressDeleted
 }
 
 internal sealed record AddressChange
 {
-    public Guid Id { get; init; }
+    public Guid UnitAddressId { get; init; }
     public Guid EventId { get; init; }
     public AddressChangeType ChangeType { get; init; }
-    public string Before { get; init;}
-    public string After { get; init;}
+    public DateTime? ExternalUpdated { get; init; }
+    public string? Before { get; init;}
+    public string? After { get; init;}
 
     public AddressChange(
-        Guid id,
+        Guid unitAddressId,
         Guid eventId,
         AddressChangeType changeType,
-        string before,
-        string after)
+        DateTime? externalUpdated,
+        string? before,
+        string? after)
     {
-        Id = id;
+        UnitAddressId = unitAddressId;
         EventId = eventId;
         ChangeType = changeType;
+        ExternalUpdated = externalUpdated;
         Before = before;
         After = after;
+    }
+}
+
+internal static class UnitAddressChangeConvert
+{
+    public static AddressChange Created(
+        Guid unitAddressId,
+        Guid eventId,
+        DateTime? externalUpdated)
+    {
+        return new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressCreated,
+            externalUpdated: externalUpdated,
+            before: null,
+            after: null);
+    }
+
+    public static AddressChange AccessAddressIdChanged(
+        Guid unitAddressId,
+        Guid eventId,
+        DateTime? externalUpdated,
+        Guid before,
+        Guid after)
+    {
+        return new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressAccessAddressIdChanged,
+            externalUpdated: externalUpdated,
+            before: before.ToString(),
+            after: after.ToString());
+    }
+
+    public static AddressChange StatusChanged(
+        Guid unitAddressId,
+        Guid eventId,
+        DateTime? externalUpdated,
+        UnitAddressStatus statusBefore,
+        UnitAddressStatus statusAfter)
+    {
+        return new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressStatusChanged,
+            externalUpdated: externalUpdated,
+            before: Enum.GetName(typeof(UnitAddressStatus), statusBefore),
+            after: Enum.GetName(typeof(UnitAddressStatus), statusAfter));
+    }
+
+    public static AddressChange FloorNameChanged(
+        Guid unitAddressId,
+        Guid eventId,
+        DateTime? externalUpdated,
+        string? floorNameBefore,
+        string? floorNameAfter)
+    {
+        return new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressFloorNameChanged,
+            externalUpdated: externalUpdated,
+            before: floorNameBefore,
+            after: floorNameAfter);
+    }
+
+    public static AddressChange SuiteNameChanged(
+        Guid unitAddressId,
+        Guid eventId,
+        DateTime? externalUpdated,
+        string? suiteNameBefore,
+        string? suiteNameAfter)
+    {
+        return new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressSuiteNameChanged,
+            externalUpdated: externalUpdated,
+            before: suiteNameBefore,
+            after: suiteNameAfter);
+    }
+
+    public static AddressChange Deleted(
+        Guid unitAddressId,
+        Guid eventId,
+        DateTime? externalUpdated)
+    {
+        return new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressDeleted,
+            externalUpdated: externalUpdated,
+            before: null,
+            after: null);
     }
 }
---
+++ test/OpenFTTH.AddressChangeIndexer.Tests/AddressChangeConvertTest.cs
@@ -0,0 +1,160 @@
+using OpenFTTH.Core.Address;
+
+namespace OpenFTTH.AddressChangeIndexer;
+
+public sealed class AddressChangeConvertTest
+{
+    [Fact]
+    public void Unit_address_created_is_converted_to_address_change()
+    {
+        var unitAddressId = Guid.Parse("55113f86-b304-4ee8-945e-086a398f34ef");
+        var eventId = Guid.Parse("6665e1a1-0de2-4038-a0d0-3e155cc0d7ef");
+        var externalUpdated = DateTime.UtcNow;
+
+        var expected = new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressCreated,
+            externalUpdated: externalUpdated,
+            before: null,
+            after: null);
+
+        var result = UnitAddressChangeConvert.Created(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            externalUpdated: externalUpdated);
+
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Unit_address_access_address_id_changed_to_address_change()
+    {
+        var unitAddressId = Guid.Parse("55113f86-b304-4ee8-945e-086a398f34ef");
+        var eventId = Guid.Parse("6665e1a1-0de2-4038-a0d0-3e155cc0d7ef");
+        var externalUpdated = DateTime.UtcNow;
+        var accessAddressIdBefore = Guid.Parse("01aed8c7-28b5-4e77-ac6b-80dd84df921f");
+        var accessAddressIdAfter = Guid.Parse("6a1b141c-c2d9-4eb9-bd6f-8c73171a92ab");
+
+        var expected = new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressAccessAddressIdChanged,
+            externalUpdated: externalUpdated,
+            before: accessAddressIdBefore.ToString(),
+            after: accessAddressIdAfter.ToString());
+
+        var result = UnitAddressChangeConvert.AccessAddressIdChanged(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            externalUpdated: externalUpdated,
+            before: accessAddressIdBefore,
+            after: accessAddressIdAfter);
+
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Unit_address_status_changed_to_address_change()
+    {
+        var unitAddressId = Guid.Parse("55113f86-b304-4ee8-945e-086a398f34ef");
+        var eventId = Guid.Parse("6665e1a1-0de2-4038-a0d0-3e155cc0d7ef");
+        var externalUpdated = DateTime.UtcNow;
+        var statusBefore = UnitAddressStatus.Pending;
+        var statusAfter = UnitAddressStatus.Active;
+
+        var expected = new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressStatusChanged,
+            externalUpdated: externalUpdated,
+            before: Enum.GetName(typeof(UnitAddressStatus), statusBefore),
+            after: Enum.GetName(typeof(UnitAddressStatus), statusAfter));
+
+        var result = UnitAddressChangeConvert.StatusChanged(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            externalUpdated: externalUpdated,
+            statusBefore: statusBefore,
+            statusAfter: statusAfter);
+
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Unit_address_floor_name_changed_to_address_change()
+    {
+        var unitAddressId = Guid.Parse("55113f86-b304-4ee8-945e-086a398f34ef");
+        var eventId = Guid.Parse("6665e1a1-0de2-4038-a0d0-3e155cc0d7ef");
+        var externalUpdated = DateTime.UtcNow;
+        var floorNameBefore = "2b";
+        var floorNameAfter = "3b";
+
+        var expected = new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressFloorNameChanged,
+            externalUpdated: externalUpdated,
+            before: floorNameBefore,
+            after: floorNameAfter);
+
+        var result = UnitAddressChangeConvert.FloorNameChanged(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            externalUpdated: externalUpdated,
+            floorNameBefore: floorNameBefore,
+            floorNameAfter: floorNameAfter);
+
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Unit_address_suite_name_changed_to_address_change()
+    {
+        var unitAddressId = Guid.Parse("55113f86-b304-4ee8-945e-086a398f34ef");
+        var eventId = Guid.Parse("6665e1a1-0de2-4038-a0d0-3e155cc0d7ef");
+        var externalUpdated = DateTime.UtcNow;
+        string? suiteNameBefore = null;
+        var suiteNameAfter = "mf";
+
+        var expected = new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressSuiteNameChanged,
+            externalUpdated: externalUpdated,
+            before: suiteNameBefore,
+            after: suiteNameAfter);
+
+        var result = UnitAddressChangeConvert.SuiteNameChanged(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            externalUpdated: externalUpdated,
+            suiteNameBefore: suiteNameBefore,
+            suiteNameAfter: suiteNameAfter);
+
+        result.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void Unit_address_deleted_to_address_change()
+    {
+        var unitAddressId = Guid.Parse("55113f86-b304-4ee8-945e-086a398f34ef");
+        var eventId = Guid.Parse("6665e1a1-0de2-4038-a0d0-3e155cc0d7ef");
+        var externalUpdated = DateTime.UtcNow;
+
+        var expected = new AddressChange(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            changeType: AddressChangeType.UnitAddressDeleted,
+            externalUpdated: externalUpdated,
+            before: null,
+            after: null);
+
+        var result = UnitAddressChangeConvert.Deleted(
+            unitAddressId: unitAddressId,
+            eventId: eventId,
+            externalUpdated: externalUpdated);
+
+        result.Should().BeEquivalentTo(expected);
+    }
+}
